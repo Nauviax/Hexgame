24767d5084960f9fe94825c3063bcca7ef1d0de1
Nauviax <nauviax@icloud.com>
2023-12-28 17:45:02 +1300

    Moved points -> p_code logic to pattern_on_grid. pattern resource now directly takes p_code

--- pattern/pattern.gd
+++ pattern/pattern.gd
@@ -20,9 +20,9 @@ var p_exe = null
 #	(And a reminder, the last bit (1s column) is the top of the stack)
 var value = 0.0
 
-# Pattern constructor. Takes list of points, then calculates p_code.
-func _init(points):
-	p_code = Pattern.calc_p_code(points)
+# Pattern constructor. Takes a code string, then sets up pattern based on that.
+func _init(p_code: String):
+	self.p_code = p_code
 	# Get pattern name and validity, then get executable code.
 	Valid_Patterns.set_pattern_name(self)
 	p_exe_name = name.to_lower().replace(" ", "_").replace("'", "").replace(":", "")
@@ -35,59 +35,6 @@ func execute(hexecutor):
 		hexecutor.stack.push_back(Bad_Iota.new())
 		return "Error: Not enough iotas in stack"
 	return p_exe.execute(hexecutor, self)
-
-# Calculates p_code from points.
-static func calc_p_code(points):
-	# Get the initial direction.
-	var dir = get_dir(points[0], points[1])
-	var dir2 = -1 # Used in for loop
-	var p_code = str(dir) # The return value. Starts with a num, then letters.
-	# For every other point, get the turn from the previous point.
-	for ii in range(2, points.size()):
-		dir2 = get_dir(points[ii-1], points[ii])
-		p_code += get_turn(dir, dir2)
-		dir = dir2
-	return p_code
-
-# Calculates the direction from point a to point b. (1-6 starting NE and going clockwise.)
-static func get_dir(a, b):
-	var x = b.x_id - a.x_id
-	var y = b.y_id - a.y_id
-	if x == 0:
-		if y == 1:
-			return 3
-		else: # y == -1
-			return 6
-	elif x == 1:
-		if y == 0:
-			return 2
-		else: # y == -1
-			return 1
-	else: # x == -1
-		if y == 0:
-			return 5
-		else: # y == 1
-			return 4
-
-# Calculates the turn from direction a to direction b. (L, l, s, r, R)
-# Inputs are 1-6 starting NE and going clockwise.
-static func get_turn(a, b):
-	var turn = b - a
-	if turn < 0: # Avoid negative numbers
-		turn += 6
-	match turn:
-		0:
-			return "s"
-		1:
-			return "r"
-		2:
-			return "R"
-		4:
-			return "L"
-		5:
-			return "l"
-		_:
-			return null # Should never happen. (This is also the "3" case.)
 
 # _to_string override for fancy display
 func _to_string():
--- pattern/pattern_on_grid.gd
+++ pattern/pattern_on_grid.gd
@@ -21,8 +21,8 @@ func _init(point_init, line_init):
 		pnt.in_use = true
 	line = line_init
 
-	# Create pattern object.
-	pattern = Pattern.new(points)
+	# Create pattern object, calculating p_code from points.
+	pattern = Pattern.new(Pattern_Ongrid.calc_p_code(points))
 
 # Remove pattern from grid, freeing up all points and removing line graphic.
 func remove():
@@ -32,3 +32,56 @@ func remove():
 	points.clear()
 	# Remove line graphic.
 	line.queue_free()
+
+# Calculates p_code from points.
+static func calc_p_code(points):
+	# Get the initial direction.
+	var dir = get_dir(points[0], points[1])
+	var dir2 = -1 # Used in for loop
+	var p_code = str(dir) # The return value. Starts with a num, then letters.
+	# For every other point, get the turn from the previous point.
+	for ii in range(2, points.size()):
+		dir2 = get_dir(points[ii-1], points[ii])
+		p_code += get_turn(dir, dir2)
+		dir = dir2
+	return p_code
+	
+# Calculates the direction from point a to point b. (1-6 starting NE and going clockwise.)
+static func get_dir(a, b):
+	var x = b.x_id - a.x_id
+	var y = b.y_id - a.y_id
+	if x == 0:
+		if y == 1:
+			return 3
+		else: # y == -1
+			return 6
+	elif x == 1:
+		if y == 0:
+			return 2
+		else: # y == -1
+			return 1
+	else: # x == -1
+		if y == 0:
+			return 5
+		else: # y == 1
+			return 4
+
+# Calculates the turn from direction a to direction b. (L, l, s, r, R)
+# Inputs are 1-6 starting NE and going clockwise.
+static func get_turn(a, b):
+	var turn = b - a
+	if turn < 0: # Avoid negative numbers
+		turn += 6
+	match turn:
+		0:
+			return "s"
+		1:
+			return "r"
+		2:
+			return "R"
+		4:
+			return "L"
+		5:
+			return "l"
+		_:
+			return null # Should never happen. (This is also the "3" case.)
