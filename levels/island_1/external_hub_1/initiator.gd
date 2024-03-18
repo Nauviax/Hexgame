09ef2e34397514cb10c9a9ce0241afd893d8a470
Nauviax <nauviax@icloud.com>
2024-03-18 17:27:59 +1300

    Fix initial hexes not being created

--- levels/island_1/external_hub_1/initiator.gd
+++ levels/island_1/external_hub_1/initiator.gd
@@ -1,8 +1,8 @@
 # External Hub 1 Initiator
 static func initiate(level_base):
 	# Create starting hexes for player, and set as sb slot 1 and 2
-	# If player spellbook is not empty, do not overwrite it.
-	if level_base.player.sb != [null, null, null, null]:
+	# If player spellbook is not empty, do not overwrite it. (Run on first enter only)
+	if level_base.player.sb != [null, null, null, null, null, null, null, null, null]:
 		return
 	# Sample hex will get the entity player is looking at, and try to enter it.
 	var hex_strings = ["1lLl", "2LL", "1lLl", "2sL", "2srLlL", "6llL"]
